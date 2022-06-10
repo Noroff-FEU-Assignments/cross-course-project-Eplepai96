a962f60b09c4e05d2239366b59a0527456bdee0e
Eplepai96 <bjorn.andreas1996@gmail.com>
2022-06-10 22:54:34 +0200

    Some more changes

---
+++ Ny mappe/js/specific.js
@@ -0,0 +1,16 @@
+// const addToCart = document.querySelector ('#add-to-cart-button')
+//     const shoppingBag = document.querySelector ('#Shopping-bag')
+//     const total = document.querySelector('data-totalitems');
+
+//     let newTotal = parseInt(total) + 1;
+
+//     console.log(total)
+
+// function addToCartAnimation(){
+
+//     total.innerHTML += newTotal
+    
+// }
+
+// addToCart.addEventListener ('click', addToCartAnimation)
+
